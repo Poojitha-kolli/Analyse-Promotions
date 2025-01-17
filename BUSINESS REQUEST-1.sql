210c27f188ea9ac8facbc9f2e25b691d7b97fcde
Poojitha Kolli <103435836+Poojitha-kolli@users.noreply.github.com>
2025-01-17 22:24:51 +0530

    Create BUSINESS REQUEST-1.sql

---
+++ BUSINESS REQUEST-1.sql
@@ -0,0 +1,28 @@
+CREATE VIEW vw_fact_events_details AS
+SELECT 
+    fe.event_id,
+    fe.store_id,
+    fe.campaign_id,
+    fe.product_code,
+    fe.base_price,
+    fe.promo_type,
+    fe.quantity_sold_before_promo,
+    fe.quantity_sold_after_promo,
+    ds.city,
+    dc.campaign_name,
+    dc.start_date,
+    dc.end_date,
+    dp.product_name,
+    dp.category
+FROM fact_events fe
+INNER JOIN dim_stores ds ON fe.store_id = ds.store_id
+INNER JOIN dim_campaigns dc ON fe.campaign_id = dc.campaign_id
+INNER JOIN dim_products dp ON fe.product_code = dp.product_code;
+
+SELECT * FROM vw_fact_events_details;
+
+/*SQL QUERY 1 */
+
+SELECT distinct product_name, base_price, promo_type
+FROM vw_fact_events_details
+WHERE base_price > 500 AND promo_type = 'BOGOF';
